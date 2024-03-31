b09383ecf2d64cadc1a646b778a8fde47819f286
Wouter Missler <woutmissler2@gmail.com>
2024-03-31 23:58:12 +0200

    small completed games filter

--- src/components/game/game-list.tsx
+++ src/components/game/game-list.tsx
@@ -4,10 +4,13 @@ import { getAllGames, startGame } from "@/lib/data";
 import GameCard from "./game-card";
 import { Button } from "../ui/button";
 import { useEffect, useState } from "react";
-import { GameProgress } from "@/lib/definitions";
+import { GameProgress, GameState } from "@/lib/definitions";
+import { toast } from "sonner";
+import { Toggle } from "../ui/toggle";
 
 export default function GameList() {
     const [games, setGames] = useState<GameProgress[]>([]);
+    const [showCompleted, setShowCompleted] = useState(false);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -23,18 +26,38 @@ export default function GameList() {
     async function handleStartGame() {
         const game = await startGame();
         setGames((games) => [game, ...games]);
+        toast.success("Your game has started with ID " + game.gameId);
+    }
+
+    function getGamesToShow() {
+        return games
+            .filter(
+                (game) => game.state !== GameState.ELIMINATED || showCompleted
+            )
+            .sort(
+                (a, b) =>
+                    new Date(b.createDate).getTime() -
+                    new Date(a.createDate).getTime()
+            );
     }
 
     return (
         <>
-            <Button className="mb-12" onClick={handleStartGame}>
-                Start a new game
-            </Button>
+            <div className="flex items-center space-x-4 mb-12">
+                <Button onClick={handleStartGame}>Start a new game</Button>
+                <Toggle
+                    onPressedChange={(toggleState) => {
+                        setShowCompleted(toggleState);
+                    }}
+                >
+                    Show completed games
+                </Toggle>
+            </div>
             {loading ? (
                 <div>Loading...</div>
             ) : (
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                    {games.map((game) => (
+                    {getGamesToShow().map((game) => (
                         <GameCard key={game.gameId} game={game} />
                     ))}
                 </div>
